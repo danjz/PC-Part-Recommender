0344d9814d5d0f7f8ee572bf1dd4dc3fa7b0269e
Bruno Mila <milaruizdeapodaca@cardiff.ac.uk>
2021-04-22 18:47:43 +0100

    Added controlled vocabulary python file, modified ReadMe.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Informatics
 
+Group Members:
+
+Dan Zwirek - zwirekd@cardiff.ac.uk
+
+Tito Thomas - thomasoo@cardiff.ac.uk
+
+Salman Linjawi - linjawis@cardiff.ac.uk
+
+Alejandro Arroyo - arroyoa@cardiff.ac.uk
+
+Bruno Mila Ruiz De Apodaca - milaruizdeapodacab@cardiff.ac.uk
---
+++ controlledvocabulary.py
@@ -0,0 +1,8 @@
+controlledvocabulary = {
+    "PC Components" : ["Processor", "Storage", "Graphics Card", "RAM", 
+                        "Motherboard","Fan", "Wireless Network Card", "Power Supply"],
+
+    "PC Peripheries" : ["Case","Monitor"],
+
+    "Price" : ["Low", "Medium", "High"]
+}  
