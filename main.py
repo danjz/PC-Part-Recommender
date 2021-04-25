391f5bf4c570c90ea91a656db72ad4bc91c7609f
Salman Linjawi <linjawis@cardiff.ac.uk>
2021-04-25 13:12:38 +0000

    Added conditons to User() but don't think it matters that much - since marking will not be looked based on code quality! I've scrapped down the flask/python webpage user interface and going with text-based command line - Tito pretty much covered user interface in recommend.py so job well done. Unless we wanna add more options (e.g. search) in the code, Think its done.

---
+++ main.py
@@ -0,0 +1,93 @@
+
+
+from pcpartpicker import API
+import cgi
+
+# api: https://github.com/JonathanVusich/pcpartpicker/
+form = cgi.FieldStorage()
+api = API("uk")
+all_data = api.retrieve("cpu", "case", "cpu-cooler", "video-card", "motherboard", "memory", "internal-hard-drive",
+                        "power-supply")
+
+
+def User():
+    count = 0
+
+    while count == 0:
+        purpose = int(
+            input("Will you be using your system for (1 or 2): \n 1. Gaming \n 2. Content Creation/Video Editing:\n"))
+        if purpose in {1, 2}:
+            budget = int(input("Please enter your budget (£): "))
+# Must update division - 'time' and 'exp' are not taken into consideration atm must update but not sure expression<---
+            time = input("Do you mainly play at night (n) r during the day (d)")
+            exp = input("Are you buying PC parts for the first time (y/n)")
+            division(budget, purpose)
+            count = count + 1
+
+
+
+
+
+
+def division(budget, purpose):
+    relparts = []
+    current_cost = 0
+    if purpose == 1:
+        mincpu, maxcpu = 0.2 * budget, 0.25 * budget
+        mingpu, maxgpu, = 0.25 * budget, 0.3 * budget
+        mincase, maxcase = 0.03 * budget, 0.06 * budget
+        mincooler, maxcooler = 0.025 * budget, 0.035 * budget
+        minmobo, maxmobo = 0.09 * budget, 0.15 * budget
+        minmem, maxmem = 0.06 * budget, 0.1 * budget
+        minhdd, maxhdd = 0.04 * budget, 0.07 * budget
+        minpsu, maxpsu = 0.04 * budget, 0.08 * budget
+    else:
+        mincpu, maxcpu = 0.25 * budget, 0.30 * budget,
+        mingpu, maxgpu, = 0.10 * budget, 0.14 * budget,
+        mincase, maxcase = 0.03 * budget, 0.06 * budget
+        mincooler, maxcooler = 0.025 * budget, 0.035 * budget
+        minmobo, maxmobo = 0.09 * budget, 0.1 * budget
+        minmem, maxmem = 0.1 * budget, 0.12 * budget
+        minhdd, maxhdd = 0.1 * budget, 0.15 * budget
+        minpsu, maxpsu = 0.04 * budget, 0.06 * budget
+
+    items = {
+        "cpu": [mincpu, maxcpu],
+        "case": [mincase, maxcase],
+        "cpu-cooler": [mincooler, maxcooler],
+        "video-card": [mingpu, maxgpu],
+        "motherboard": [minmobo, maxmobo],
+        "memory": [minmem, maxmem],
+        "internal-hard-drive": [minhdd, maxhdd],
+        "power-supply": [minpsu, maxpsu]}
+
+    for key in all_data:
+        added = 0
+        counter = 0
+        index = list(items).index(key)
+        relparts.append([])
+        for i in all_data[key]:
+            # use ".amount"
+
+            if items[key][0] < i.price.amount < items[key][1] and counter < 6:
+                # print(i)
+                relparts[index].append(i)
+                added += 1
+                counter += 1
+
+    # print(relparts[0])
+    if len(relparts) != 0:
+        print("We recommend the following parts:")
+
+        for item in relparts:
+            index = relparts.index(item)
+            print("\n")
+            print(list(items)[index].upper())
+            print("==================================")
+            if len(item) == 0:
+                print("We dont have that part right now")
+            else:
+                for i in item:
+                    # print(i)
+                    print(i.brand, i.model, "|" + " Price: ", i.price)
+User()
